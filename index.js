58e3b448fa402c976e446b811d15f9f11ed7f6e1
tonyng <tonyng987@gmail.com>
2019-12-13 14:31:45 -0800

    Add login,logout, & register mutations

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = require('./server/server');
 // const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
