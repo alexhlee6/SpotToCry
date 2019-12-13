ce721013406bdc48d6d8c2d097cbdadc49c3901c
tonyng <tonyng987@gmail.com>
2019-12-13 14:12:55 -0800

    Add user auth mutations, models, & type

---
+++ server/schema/mutations.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID } = graphql;
+
+const UserType = require("./types/user_type");
+const AuthService = require("../services/auth");
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    register: {
+      type: UserType,
+      args: {
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(_, args) {
+        return AuthService.register(args);
+      }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(_, args) {
+        return AuthService.login(args);
+      }
+    },
+    logout: {
+      type: UserType,
+      args: {
+        _id: { type: GraphQLID }
+      },
+      resolve(_, args) {
+        return AuthService.logout(args);
+      }
+    },
+    verifyUser: {
+      type: UserType,
+      args: {
+        token: { type: GraphQLString }
+      },
+      resolve(_, args) {
+        return AuthService.verifyUser(args);
+      }
+    }
+  }
+});
+
+module.exports = mutation;
