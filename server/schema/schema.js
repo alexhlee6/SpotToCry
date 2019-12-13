3d69dc25b7c8b35c8f9f15064387d03ea5ebeac0
Seth Ullman <seth.ullman98@gmail.com>
2019-12-13 14:57:02 -0700

    add schema.js

---
+++ server/schema/schema.js
@@ -0,0 +1,10 @@
+const graphql = require('graphql');
+const { GraphQLSchema } = graphql;
+
+const queries = require('');
+const mutations = require('');
+
+module.export = new GraphQLSchema({
+  queries,
+  mutations
+})
