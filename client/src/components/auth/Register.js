c91e5f180a0d43d7b646d89e6b7ed5740bef102f
tonyng <tonyng987@gmail.com>
2019-12-13 22:55:28 -0800

    Style register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Mutation } from "react-apollo";
 import Mutations from "../../graphql/mutations";
+import { Link } from "react-router-dom";
 
 const { REGISTER_USER } = Mutations;
 
@@ -35,42 +36,90 @@ class Register extends Component {
         }}
         update={(client, data) => this.updateCache(client, data)}
       >
+        {/* <div className="signup">
+          <Link to="/">
+            <img
+              id="signup-logo"
+              src={"Amplify_BW_Transparent.png"}
+              alt="logo"
+            ></img>
+          </Link>
+          <div className="content">
+            <button id="signup-fb">Sign in as demo_user</button> */}
         {registerUser => (
-          <div>
-            <form
-              onSubmit={e => {
-                e.preventDefault();
-                registerUser({
-                  variables: {
-                    name: this.state.name,
-                    email: this.state.email,
-                    password: this.state.password
-                  }
-                });
-              }}
-            >
-              <input
-                value={this.state.name}
-                onChange={this.update("name")}
-                placeholder="Name"
-              />
-              <input
-                value={this.state.email}
-                onChange={this.update("email")}
-                placeholder="Email"
-              />
-              <input
-                value={this.state.password}
-                onChange={this.update("password")}
-                type="password"
-                placeholder="Password"
-              />
-              <button type="submit">Register</button>
-            </form>
+          <div className="signup">
+            <div className="signup-Header">
+              <Link to="/">
+                <img
+                  id="signup-logo"
+                  src={"Amplify_BW_Transparent.png"}
+                  alt="logo"
+                ></img>
+              </Link>
+            </div>
+            <div className="content">
+              <button id="signup-fb">Log in as demo user</button>
+              <form
+                onSubmit={e => {
+                  e.preventDefault();
+                  registerUser({
+                    variables: {
+                      name: this.state.name,
+                      email: this.state.email,
+                      password: this.state.password
+                    }
+                  });
+                }}
+              >
+                <br />
+                <strong className="line-thru">or</strong>
+                <h2 className="signup-h2">Sign up with your email address</h2>
+                <div className="login-form">
+                  <br />
+                  <label>
+                    <input
+                      value={this.state.name}
+                      onChange={this.update("name")}
+                      className="input-register-1"
+                      placeholder="Name"
+                    />
+                  </label>
+                  <br />
+                  <label>
+                    <input
+                      value={this.state.email}
+                      onChange={this.update("email")}
+                      className="input-register-1"
+                      placeholder="Email"
+                    />
+                  </label>
+                  <br />
+                  <label>
+                    <input
+                      value={this.state.password}
+                      onChange={this.update("password")}
+                      className="input-register-1"
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </label>
+                  <br />
+                  <button type="submit" className="signup-submit">Sign Up</button>
+                  <div className="login-prompt">
+                    Already have an account?
+                    <Link id="login-highlight" to="/login">
+                      Log in
+                    </Link>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
         )}
+        {/* </div>
+        </div> */}
       </Mutation>
-    )
+    );
   }
 }
 
