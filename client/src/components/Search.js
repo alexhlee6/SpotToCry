edbfdefc7e039b7fa8e86aa627712688bffeb9ec
Seth Ullman <seth.ullman98@gmail.com>
2019-12-16 11:49:56 -0700

    add unstyled searchbar

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -8,6 +8,13 @@ class SearchBar extends React.Component{
   render(){
     return (
       <div className='search'>
+        <div className='search-bar'>
+          <input
+            className='search-input'
+            placeholder='Search for Artists or Songs'
+          />
+          
+        </div>
         I am the Search Component!
       </div>
     )
