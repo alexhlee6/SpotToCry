af0641199908685fa5c88b4f832c4a50f76a133f
alex629lee <alex629lee@yahoo.com>
2019-12-16 19:01:36 -0800

    Add artist info to currently playing song

--- client/src/components/player/CurrentSongShow.jsx
+++ client/src/components/player/CurrentSongShow.jsx
@@ -1,39 +1,90 @@
 import React from "react";
+import { Query } from "react-apollo";
+import Queries from "../../graphql/queries";
 
 class CurrentSongShow extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = props.song;
+  
+    this.state = {
+      songId: this.props.songId,
+      bSide: false
+    }
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps !== this.props) {
-      this.setState(this.props.song)
+      this.setState({
+        songId: this.props.songId,
+        bSide: false
+      })
     }
   }
 
   render() {
-    let image;
-    if (this.state.imageUrl) {
-      image = <img className="current-song-image" src={this.state.imageUrl} /> 
-    }
-
-    let mainInfo;
-    if (this.state.title) {
-      mainInfo = (
-        <div>
-          <p>{this.state.title}</p>
-          <p>{this.state.artist}</p>
-        </div>
-      )
-    }
-
     return (
-      <div className="current-song-image-container">
-        { image }
-        { mainInfo }
-      </div>
+      <Query query={ Queries.FETCH_SONG } variables={{ id: this.state.songId }}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error</p>;
+
+          if (!this.state.bSide) {
+            return (
+              <div className="current-song-show-main">
+                <div className="current-song-show-A">
+                  <div className="current-song-image-container">
+                    <img className="current-song-image" 
+                      src={data.song.imageUrl} 
+                      onClick={() => this.setState({ bSide: true })}
+                    />
+                  </div>
+                  <div className="current-song-info">
+                    <p>{data.song.title}</p>
+                    <p>{data.song.artist.name}</p>
+                  </div>
+                  <div
+                    className="current-song-show-more-button"
+                    onClick={() => this.setState({ bSide: true })}
+                  >
+                    About {data.song.artist.name}
+                  </div>
+                </div>
+              </div>
+            )
+          } else {
+            return (
+              <div className="current-song-show-main">
+                <div className="current-song-show-B">
+                  <div className="current-song-image-container">
+                    <img className="current-song-image" 
+                      src={data.song.artist.imageUrl} 
+                      onClick={() => this.setState({ bSide: false })}
+                    />
+                  </div>
+                  <div className="current-artist-info">
+                    <p>{data.song.artist.name}</p>
+                    <div className="current-artist-genres">
+                      {data.song.artist.genres.map(
+                        genre => <p>{genre.name}</p>
+                      )}
+                    </div>
+                    <p className="current-artist-description">
+                      {data.song.artist.description}
+                    </p>
+                  </div>
+                  <div
+                    className="current-song-show-more-button"
+                    onClick={() => this.setState({ bSide: false})}
+                  >
+                    Back to Song
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        }}
+      </Query>
     )
   }
 }
