2646079eddcf15c09b046370b1981cc04dc48964
Seth Ullman <seth.ullman98@gmail.com>
2019-12-18 16:22:46 -0700

    add pictures to song and artist items

--- client/src/components/Artist_item.jsx
+++ client/src/components/Artist_item.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 const ArtistItem = (artist) => {
+  const artistStyle = {
+    marginTop: '20px',
+    height: '130px',
+    width: '130px',
+    backgroundImage: `url(${artist.imageUrl})`,
+    backgroundSize: 'cover',
+    backgroundPostition: 'center'
+  }
   return (
     <div className='artist-item'>
-      <div className='artist-image'>
+      <div 
+        className='artist-image'
+        style={artistStyle}
+        >
       </div>
       <div className='artist-info'>
         <p className='info-name'>{artist.name}</p>
