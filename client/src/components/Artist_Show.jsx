7e4bc4045fc17028160982b14378ef5e3e0edae0
Seth Ullman <seth.ullman98@gmail.com>
2019-12-19 14:10:05 -0700

    add individual artists show pages with songs

--- client/src/components/Artist_Show.jsx
+++ client/src/components/Artist_Show.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
+import { ApolloConsumer, Query } from 'react-apollo';
+import queries from '../graphql/queries';
+const { FETCH_ALL_ARTISTS } = queries;
 
 class ArtistShow extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      songs: []
+    }
   }
 
   render(){
-    const artistId = window.location.href.split('/')[5];
+    const artistId = window.location.href.split('/').slice(-1)[0];
     
     return(
       <div>
-        {`Welcome to the ${artistId} show page!`}
+        <Query query={FETCH_ALL_ARTISTS}>
+          {({ loading, error, data }) => {
+            // debugger;
+            if (loading) return null;
+            if (error) return `Error! ${error}`;
+            for(let i = 0; i < data.artists.length; i++){
+              if (data.artists[i]._id === artistId){
+                this.artist = data.artists[i];
+              }
+            }
+            return (
+              <div className='artist-show'>
+                <div className='artist-banner'>
+                  
+                </div>
+                <div className='artist-songs'>
+
+                </div>
+              </div>
+             );
+          }}
+        </Query>}
       </div>
+      
     );
   }
 }
