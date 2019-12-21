55e797a38195f87480000ddaaab8961a882e6664
alex629lee <alex629lee@yahoo.com>
2019-12-20 18:07:03 -0800

    Push to heroku

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-
-const path = require('path');
 const app = require('./server/server');
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
 
 const port = process.env.PORT || 5000;
 
