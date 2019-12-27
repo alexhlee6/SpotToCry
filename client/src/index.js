f5f3db40adf6610584c4413909813da2f9625d7c
alex629lee <alex629lee@yahoo.com>
2019-12-26 21:39:10 -0800

    Undo heroku push changes

--- client/src/index.js
+++ client/src/index.js
@@ -20,7 +20,7 @@ const cache = new InMemoryCache({
 });
 
 const httpLink = createHttpLink({
-  // uri: "http://localhost:5000/graphql",
+  uri: "http://localhost:5000/graphql",
   headers: {
     // pass our token into the header of each request
     authorization: localStorage.getItem("auth-token")
