385ec4358ca3bc350f0639b3dac63eb17dac71dd
tonyng <tonyng987@gmail.com>
2019-12-27 17:42:20 -0800

    Fix current user

--- client/src/index.js
+++ client/src/index.js
@@ -15,86 +15,87 @@ import resolvers from "./resolvers";
 import { ApolloLink} from "apollo-link"
 const { VERIFY_USER } = Mutations;
 
-const cache = new InMemoryCache({
-  dataIdFromObject: object => object._id || null
-});
+let client;
+let cache;
 
-const httpLink = createHttpLink({
-  uri: "http://localhost:5000/graphql",
-  headers: {
-    // pass our token into the header of each request
-    authorization: localStorage.getItem("auth-token")
-  }
-});
+async function setupClient() {
+    cache = new InMemoryCache({
+    dataIdFromObject: object => object._id || null
+  });
 
-// make sure we log any additional errors we receive
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) graphQLErrors.map(({ message }) => console.log(message));
-  if (networkError) {
-    // debugger;
-    console.log(networkError);
-  }
-});
+  const httpLink = createHttpLink({
+    uri: "http://localhost:5000/graphql",
+    headers: {
+      // pass our token into the header of each request
+      authorization: localStorage.getItem("auth-token")
+    }
+  });
 
-const link = ApolloLink.from([
-  errorLink, 
-  httpLink
-])
+  // make sure we log any additional errors we receive
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) graphQLErrors.map(({ message }) => console.log(message));
+    if (networkError) {
+      console.log(networkError);
+    }
+  });
 
-const client = new ApolloClient({
-  resolvers,
-  link,
-  cache,
-  // context: ({ req }) => {
-  //   // get the user token from the headers
-  //   const token = req.headers.authorization || '';
+  const link = ApolloLink.from([
+    errorLink, 
+    httpLink
+  ])
 
-  //   // try to retrieve a user with the token
-  //   const user = getUser(token);
-
-  //   // add the user to the context
-  //   return { user };
-  // },
-  onError: ({ networkError, graphQLErrors }) => {
-    console.log("graphQLErrors", graphQLErrors);
-    console.log("networkError", networkError);
-  }
-});
-
-const token = localStorage.getItem("auth-token");
-cache.writeData({
-  data: {
-    isLoggedIn: Boolean(token),
-    isModalOpen: false
-  }
-});
-
-if (token) {
-  client
-    // use the VERIFY_USER mutation directly use the returned data to know if the returned
-    // user is loggedIn
-    .mutate({ mutation: VERIFY_USER, variables: { token } })
-    .then(({ data }) => {
-      cache.writeData({
-        data: {
-          isLoggedIn: data.verifyUser.loggedIn,
-          currentUser: data.verifyUser._id
-        }
-      });
-    });
+  client = new ApolloClient({
+    resolvers,
+    link,
+    cache,
+    onError: ({ networkError, graphQLErrors }) => {
+      console.log("graphQLErrors", graphQLErrors);
+      console.log("networkError", networkError);
+    }
+  });
 }
 
-const Root = () => {
-  return (
-    <ApolloProvider client={client}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </ApolloProvider>
-  );
-};
+async function populateCache() {
+  const token = localStorage.getItem("auth-token");
+  cache.writeData({
+    data: {
+      isLoggedIn: Boolean(token),
+      isModalOpen: false
+    }
+  });
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+  if (token) {
+    await client
+      // use the VERIFY_USER mutation directly use the returned data to know if the returned
+      // user is loggedIn
+      .mutate({ mutation: VERIFY_USER, variables: { token } })
+      .then(({ data }) => {
+        debugger;
+        cache.writeData({
+          data: {
+            isLoggedIn: data.verifyUser.loggedIn,
+            currentUser: data.verifyUser._id 
+          }
+        });
+      });
+  }
+}
+
+setupClient()
+  .then(() => populateCache())
+  .then(() => {
+    const Root = () => {
+      return (
+        <ApolloProvider client={client}>
+          <HashRouter>
+            <App />
+          </HashRouter>
+        </ApolloProvider>
+      );
+    };
+
+    ReactDOM.render(<Root />, document.getElementById("root"));
+})
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
